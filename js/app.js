18fb09f494b172d760c0bf7cfa9b997ca5882272
David Blackman-Mathis <dbmathis@bluestatedigital.com>
2014-10-27 16:34:00 -0400

    Some simple models.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
-App = Ember.Application.create();
+BSDEM = Ember.Application.create();
 
-App.Router.map(function() {
+BSDEM.Router.map(function() {
   // put your routes here
 });
 
-App.IndexRoute = Ember.Route.extend({
+BSDEM.IndexRoute = Ember.Route.extend({
   model: function() {
     return ['red', 'yellow', 'blue'];
   }
 });
+
+var attr = DS.attr();
+
+BSDEM.AppFormField = DS.Model.extend({
+    id: attr,
+    app_form_id: attr,
+    display_order: attr,
+    type: attr,
+    settings: DS.belongsTo(BSDEM.AppFormFieldSettings,{embedded:'always'})
+})
+
+BSDEM.AppFormFieldSettings = DS.Model.extend({
+    label: attr,
+    isRequired: attr,
+    isIncludedOnTaxReciept: attr,
+    valueIfBlank: attr,
+    valueIfChecked: attr,
+    valueIfUnchecked: attr,
+    name: attr
+})
+
