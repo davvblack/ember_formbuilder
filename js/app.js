446d05546dce7c4c4a4944153280e610a8c94b7b
David Blackman-Mathis <dbmathis@bluestatedigital.com>
2014-10-27 20:49:14 -0400

    new model updates

--- js/app.js
+++ js/app.js
@@ -1,32 +1,49 @@
+/*
+ BSDEM.Router.map(function() {
+ // put your routes here
+ });
+
+ BSDEM.IndexRoute = Ember.Route.extend({
+ model: function() {
+ return ['red', 'yellow', 'blue'];
+ }
+ });
+ */
+
 BSDEM = Ember.Application.create();
-
-BSDEM.Router.map(function() {
-  // put your routes here
-});
-
-BSDEM.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
 
 var attr = DS.attr();
 
+/* This won't be posted by the app. */
+BSDEM.AppForm = DS.Model.extend({
+    id: attr,
+    app_form_fields: DS.hasMany(BSDEM.AppFormField)
+})
+
 BSDEM.AppFormField = DS.Model.extend({
     id: attr,
-    app_form_id: attr,
+    app_form_id: DS.belongsTo(BSDEM.AppForm),
     display_order: attr,
     type: attr,
-    settings: DS.belongsTo(BSDEM.AppFormFieldSettings,{embedded:'always'})
+    settings: DS.belongsTo(BSDEM.AppFormFieldSettings, {embedded:'always'}),
+    is_new: attr
 })
 
 BSDEM.AppFormFieldSettings = DS.Model.extend({
     label: attr,
+    name: attr,
     isRequired: attr,
-    isIncludedOnTaxReciept: attr,
     valueIfBlank: attr,
     valueIfChecked: attr,
     valueIfUnchecked: attr,
-    name: attr
+    default: attr,
+    options: attr
+    isIncludedOnTaxReciept: attr,
 })
 
+BSDEM.AppForm.reopenClass({
+    FIXTURES:{
+        id: 1,
+        app_form_fields: []
+    }
+})
